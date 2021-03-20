ccf42db18afe068a70bd58545274cea7b32a30a7
kymko <Kymkobek@gmail.com>
2021-03-20 15:12:50 +0300

    Добавил метод

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -5,24 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.graphics.Color.*;
+
 public class LoginActivity extends AppCompatActivity {
 
-    TextView textEnter;
-    TextView textWelcome;
-    TextView textAction;
-    TextView textRegister;
     EditText textEmail;
     EditText textPassword;
     Button btn;
-    TextView textForgot;
-    public int counter = 3;
+    //public int counter = 3;
 
 
     @Override
@@ -30,35 +26,52 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        textEnter = findViewById(R.id.textEnter);
-        textWelcome = findViewById(R.id.textWelcome);
-        textAction = findViewById(R.id.textAction);
-        textRegister = findViewById(R.id.textRegister);
         textEmail = findViewById(R.id.textEmail);
         textPassword = findViewById(R.id.textPassword);
         btn = findViewById(R.id.button);
-        textForgot = findViewById(R.id.textForgot);
-        textEmail.setBackgroundColor(Color.WHITE);
+        textEmail.setBackgroundColor(WHITE);
 
 
         String inputName = textEmail.getText().toString();
         String inputPassword = textPassword.getText().toString();
 
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validate(inputName,inputPassword);
-
+                if (validate2()) {
+                    validate1(inputName, inputPassword);
+                }
             }
         });
-
     }
-    public void validate(String userName, String userPassword) {
+
+
+    public void validate1(String userName, String userPassword) {
+
+        btn.setBackgroundColor(Color.GREEN);
+
         if ((userName.equals("")) && (userPassword.equals(""))) {
             Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
             startActivity(intent);
-            Toast toast = Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_LONG);
+            Toast toast = Toast.makeText(this, "Добро пожаловать!", Toast.LENGTH_SHORT);
             toast.show();
         }
+    }
+
+    public Boolean validate2() {
+
+        boolean result = false;
+
+        String name = textEmail.getText().toString();
+        String pasw = textPassword.getText().toString();
+
+        if ((name.isEmpty()) && (pasw.isEmpty())) {
+            Toast toast = Toast.makeText(this, "Заполните все поля!!!", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            result = true;
+        }
+        return result;
     }
 }
